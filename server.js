6de340c63551fcb0ea3bde2d33f9775bc4ca9f6d
saurabh singh panchal <saurabh_panchal@crgl-thirdparty.com>
2020-08-27 09:56:35 +0530

    seting up deploy process

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
 const app = express();
-
+const path = require("path");
 // connect data base
 connectDB();
 
@@ -11,13 +11,20 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 
 //Define Routes
-app.get("/", (req, res) => res.send("API Running"));
-
 app.use("/api/employees", require("./routes/api/employees"));
 app.use("/api/feedbackrequests", require("./routes/api/requestPool"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/feedback", require("./routes/api/feedback"));
 
+// serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("__dirname", "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server is started on port-${PORT}`));
